66d1cf2c28fbbdf4fb3039e441c4066d5b7df0dd
Jeremy Johnson <modernclassicgamer@gmail.com>
2023-04-14 18:52:47 +0000

    minor updates

--- proxy-switcher.sh
+++ proxy-switcher.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 function on {
-  echo -e "\e[32mSet-up the proxy:\e[0m ${proxy}"
+    echo -e "\e[32mEnable proxy:\e[0m ${proxy}"
 
-  export http_proxy="${proxy}"
-  export https_proxy="${proxy}"
-  export HTTP_PROXY="${proxy}"
-  export HTTPS_PROXY="${proxy}"
+    export http_proxy="${proxy}"
+    export https_proxy="${proxy}"
+    export HTTP_PROXY="${proxy}"
+    export HTTPS_PROXY="${proxy}"
 }
 
 function off {
-  echo -e "\e[32mRemove the proxy\e[0m"
+    echo -e "\e[32mDisable proxy\e[0m"
 
-  unset http_proxy
-  unset https_proxy
-  unset HTTP_PROXY
-  unset HTTPS_PROXY
+    unset http_proxy
+    unset https_proxy
+    unset HTTP_PROXY
+    unset HTTPS_PROXY
 }
 
 if [[ $1 != "" ]]; then
-  proxy=$1
-  off
-  on
+    proxy=$1
+    on
 else
-  off
+    off
 fi
